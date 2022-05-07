0cf39ad56e1c8cb20dfbfa5828ebec940d1dd1d0
Ed Bartosh <eduard.bartosh@intel.com>
2022-05-07 16:13:05 +0300

    add DesiredStateOfWorldPopulator

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -754,7 +754,7 @@ func NewMainKubelet(kubeCfg *kubeletconfiginternal.KubeletConfiguration,
 
 	// setup resourceManager
 	if utilfeature.DefaultFeatureGate.Enabled(features.DynamicResourceAllocation) {
-		klet.resourceManager = resourcemanager.NewResourceManager(nodeName)
+		klet.resourceManager = resourcemanager.NewResourceManager(nodeName, klet.podManager)
 	}
 
 	// setup volumeManager
